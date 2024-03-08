e33b3f3767e4fe74a5a75e30bac3f225c6d90b1d
moisedk <moisedete@gmail.com>
2024-03-08 01:01:52 -0500

    reformat filter.c

--- filter.c
+++ filter.c
@@ -24,14 +24,18 @@ bool filter(const char *path, const Settings *settings)
 {
     char *base_name = basename(path);
 
-    bool _type = (is_dir(path) && settings->type == 0) || (!is_dir(path) && settings->type == 1); // path points to dir but file was searched for, or vice versa
-    bool _empty = !is_empty(path) && settings->empty;
-    bool _name = settings->name && fnmatch(settings->name, base_name, FNM_NOESCAPE) != 0;
-    bool _path = settings->path && fnmatch(settings->path, path, FNM_NOESCAPE) != 0;
-    bool _perm = settings->perm && ((to_decimal(settings->perm) & 0777) != (get_perm_mode(path) & 0777));
-    bool _access = settings->access && get_access(path, settings->access);
-    bool _mtime = settings->newer && (settings->newer > get_mtime(path));
-    bool _uid = (settings->uid != -1) && (getuid() != settings->uid);
-    bool _gid = (settings->uid != -1) && (getgid() != settings->gid);
+    bool 
+        _type = (is_dir(path) && settings->type == 0) || (!is_dir(path) && settings->type == 1), // path points to dir but file was searched for, or vice versa
+        _empty = !is_path_empty(path) && settings->empty,
+        _name = settings->name && fnmatch(settings->name, base_name, FNM_NOESCAPE) != 0,
+        _path = settings->path && fnmatch(settings->path, path, FNM_NOESCAPE) != 0,
+        _perm = settings->perm && ((to_decimal(settings->perm) & 0777) != (get_perm_mode(path) & 0777)),
+        _access = settings->access && access(path, settings->access),
+        _mtime = settings->newer && (settings->newer > get_mtime(path)),
+        _uid = (settings->uid != -1) && (getuid() != settings->uid),
+        _gid = (settings->uid != -1) && (getgid() != settings->gid);
+        
     return _type || _empty || _name || _path || _perm | _access || _mtime || _uid || _gid;
 }
+
+/* vim: set sts=4 sw=4 ts=8 expandtab ft=c: */
